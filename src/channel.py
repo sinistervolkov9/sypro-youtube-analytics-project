0c05b0aecb58dec178f892b9772964b0e699a0a9
sgse <sgse.vld@gmail.com>
2023-12-12 10:51:37 +0300

    moving a API_KEY-variable to class attribute

--- src/channel.py
+++ src/channel.py
@@ -2,17 +2,16 @@ import os
 import json
 from googleapiclient.discovery import build
 
-API_KEY: str = os.getenv('YOUTUBE_API_KEY')
-
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    API_KEY: str = os.getenv('YOUTUBE_API_KEY')
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        # Спец. объект для работы с API
-        self.youtube = build('youtube', 'v3', developerKey=API_KEY)
+        self.youtube = build('youtube', 'v3', developerKey=self.API_KEY)
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
